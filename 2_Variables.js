788058bd81df8f08b84b7d805677252713e24077
Swagata Katiyar <79069416+SwagataKatiyar@users.noreply.github.com>
2022-10-30 16:46:20 +0530

    Add files via upload

---
+++ 2_Variables.js
@@ -0,0 +1,18 @@
+//we can change variable type from one to another but this can't happen in other laguages and they throw error.
+
+var a= 67; //var is used to declare a variable
+console.log(a);
+a= "Swagata";
+console.log(a);
+
+let a= 64; //Here, 64 is literal (constant value), "=" is assignment operator, "let" is used to declare and variable and a is a "identifier" 
+
+/* Rules: 
+1. Must begin with a $, _ or a letter. 
+eg: let 8harry = 7 // Not allowed this because of indetifier is staring with number.
+
+2. We can't use reserved words
+3. JS is case sensitive
+4. Letters,digts, underscore and $ sign is only allowed.
+
+*/
