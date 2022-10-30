32e87764d980df17cb4c2ba64fadafb0a5d97c32
Swagata Katiyar <79069416+SwagataKatiyar@users.noreply.github.com>
2022-10-30 19:16:46 +0530

    Add files via upload

---
+++ 3_WaysToDeclare.js
@@ -0,0 +1,37 @@
+/*
+let:
+1. The let keyword was introduced in ES6.
+
+2. Variables defined with let cannot be Redeclared but can be updated
+
+3. Variables defined with let must be Declared before use.
+
+4. Variables defined with let have Block Scope.
+
+*/
+let x = "Swagata Katiyar";
+
+/*
+Const: 
+
+1. The const keyword was introduced in ES6 (2015).
+
+2. Variables defined with const cannot be Redeclared.
+
+3. Variables defined with const cannot be Reassigned.
+
+4. Variables defined with const have Block Scope.
+
+*/
+
+const PI = 3.141592653589793;
+
+
+/*
+var:  
+
+The var statement declares a function-scoped or globally-scoped variable, optionally initializing it to a value.
+
+*/
+
+var name= "Swagata"
