9ac9a495d72b32f53664c9d46b285e86a7100bfd
Swagata Katiyar <79069416+SwagataKatiyar@users.noreply.github.com>
2022-10-30 23:52:38 +0530

    Add files via upload

---
+++ 8_Prompt&Alert.js
@@ -0,0 +1,22 @@
+//Inspect->Console
+
+prompt("What's your age?"); /* o/p: '22' (Always return value in string when put)
+
+
+-A prompt box is used if you want the user to input a value. 
+
+-The prompt() method returns the input value if the user clicks "OK", otherwise it returns null. */
+
+  
+alert("The code is running"); /* o/p: undefined (Always)
+
+The alert() method displays an alert box with a message and an OK button.
+
+*/
+
+
+confirm("Are you 18+?"); //Returns true if the user clicked "OK", otherwise false.
+
+/*
+The confirm() method displays a dialog box with a message, an OK button, and a Cancel button.
+*/
